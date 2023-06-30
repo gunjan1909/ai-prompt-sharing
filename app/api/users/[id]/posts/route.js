5d929aa4d440a47e2a79692d285e2566f05f9136
gunjan1909 <bhanarkaranita@gmail.com>
2023-07-01 00:54:42 +0530

    .

---
+++ app/api/users/[id]/posts/route.js
@@ -0,0 +1,15 @@
+import { connectToDB } from "@utils/database";
+import Prompt from "@models/prompt";
+
+//get all posts of particular creator
+export const GET = async (req, { params }) => {
+  try {
+    await connectToDB();
+    const prompts = await Prompt.find({
+      creator: params.id,
+    }).populate("creator");
+    return new Response(JSON.stringify(prompts), { status: 200 });
+  } catch (err) {
+    return new Response(`Failed ${err.messgae}`, { status: 500 });
+  }
+};
